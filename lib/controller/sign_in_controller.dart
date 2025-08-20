91fb563b6bc9579b59017ee1a4265e2253c34b19
Owovickky <Jaunsmith2016@gmail.com>
2025-08-20 15:01:30 -0700

    sign in set up

--- lib/controller/sign_in_controller.dart
+++ lib/controller/sign_in_controller.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 
 class SignInController {
   SignInController();
 
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  bool isChecked = false;
+
+  void setChecked(bool val) {
+    isChecked = val;
+    if (kDebugMode) {
+      print('the value of val is:  $val');
+    }
+  }
+
+  void submitData() {}
 }
--- lib/screens/user/auth/signin/sign_in_screen.dart
+++ lib/screens/user/auth/signin/sign_in_screen.dart
@@ -1,10 +1,149 @@
+import 'package:dotted_line/dotted_line.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:neat_nest/controller/sign_in_controller.dart';
+import 'package:neat_nest/screens/user/auth/icon_holder.dart';
+import 'package:neat_nest/screens/user/auth/signup/sign_up_screen.dart';
+import 'package:neat_nest/screens/user/utilities/auth_text_filed.dart';
+import 'package:neat_nest/utilities/app_button.dart';
+import 'package:neat_nest/utilities/constant/colors.dart';
+import 'package:neat_nest/utilities/constant/extension.dart';
+import 'package:neat_nest/widget/app_text.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
 
   @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  late SignInController _signInScreenController;
+  bool isChecked = false;
+
+  @override
+  void didChangeDependencies() {
+    _signInScreenController = SignInController();
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back),
+          ),
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                5.ht,
+                Center(child: primaryText(text: 'Sign Up')),
+                10.ht,
+                AuthTextFiled(
+                  titleText: 'Email Address',
+                  hintText: 'Enter Email Address',
+                  textEditingController:
+                      _signInScreenController.emailController,
+                  // textEditingController: controller!,
+                ),
+                10.ht,
+                AuthTextFiled(
+                  titleText: 'Password',
+                  hintText: 'Enter Password',
+                  secure: true,
+                  textEditingController:
+                      _signInScreenController.passwordController,
+                  // textEditingController: controller!,
+                ),
+                10.ht,
+                Row(
+                  children: [
+                    Checkbox(
+                      checkColor: Colors.white,
+                      activeColor: AppColors.primaryColor,
+                      side: BorderSide(color: AppColors.primaryColor, width: 2),
+                      value: isChecked,
+                      onChanged: (val) {
+                        setState(() {
+                          isChecked = !isChecked;
+                        });
+                        _signInScreenController.setChecked(val!);
+                      },
+                    ),
+                    secondaryText(text: 'I agree to the', fontSize: 13.sp),
+                    primaryText(
+                      text: 'Terms and Conditions',
+                      color: AppColors.primaryColor,
+                      fontSize: 13.sp,
+                    ),
+                  ],
+                ),
+                20.ht,
+                AppButton(
+                  text: 'Sign In',
+                  bckColor: AppColors.primaryColor,
+                  textColor: Colors.white,
+                  width: double.infinity,
+                  fontSize: 18.sp,
+                  function: () {
+                    _signInScreenController.submitData();
+                  },
+                ),
+                30.ht,
+                DottedLine(dashColor: AppColors.secondaryTextColor),
+                20.ht,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    iconHolder(
+                      imagePath:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWryx19r0yjTs-vYzgN8-moMYY9Kf4lWDqrg&s',
+                    ),
+                    20.wt,
+                    iconHolder(
+                      imagePath:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdP6AKFlNa3Afg4RJOp7OtR7RGRrlPE2KbLg&s',
+                    ),
+                  ],
+                ),
+                30.ht,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    secondaryText(text: 'Don\'t have and account?'),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SignUpScreen(),
+                          ),
+                        );
+                      },
+                      child: secondaryText(
+                        text: 'SignUp',
+                        color: AppColors.primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
