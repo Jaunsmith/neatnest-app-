986aaee0095aa70d9c5bcd357049d939e16a4b00
Owovickky <Jaunsmith2016@gmail.com>
2025-09-15 03:19:21 -0700

    booking form

---
+++ lib/screens/booking/notifiers/booking_date_state.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'booking_date_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bookingDateStateHash() => r'f629c27e41b19a021c4e6daae9d6bcb456b42f25';
+
+/// See also [BookingDateState].
+@ProviderFor(BookingDateState)
+final bookingDateStateProvider =
+    NotifierProvider<BookingDateState, DateTime>.internal(
+      BookingDateState.new,
+      name: r'bookingDateStateProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$bookingDateStateHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$BookingDateState = Notifier<DateTime>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
---
+++ lib/screens/booking/notifiers/booking_time_state.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'booking_time_state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bookingTimeStateHash() => r'bd8a6a728c614b50ae4f9a1b01ce6c1fd45b6bfa';
+
+/// See also [BookingTimeState].
+@ProviderFor(BookingTimeState)
+final bookingTimeStateProvider =
+    NotifierProvider<BookingTimeState, String>.internal(
+      BookingTimeState.new,
+      name: r'bookingTimeStateProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$bookingTimeStateHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$BookingTimeState = Notifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
